2febf2cf0412c824077af56823375b43ee584e6d
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2024-08-07 23:47:20 +0530

    Time: 3 ms (32.16%) | Memory: 12.1 MB (18.57%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.cpp
+++ 226-invert-binary-tree/invert-binary-tree.cpp
@@ -13,13 +13,18 @@
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        if(root) {
-            TreeNode* temp = root->left;
-            root->left = root->right;
-            root->right = temp;
-            invertTree(root->left);
-            invertTree(root->right);
+        queue<TreeNode*> q;
+        if(root) q.push(root);
+        while(!q.empty()) {
+            TreeNode* cur = q.front();
+            q.pop();
+            TreeNode* temp = cur->left;
+            cur->left = cur->right;
+            cur->right = temp;
+            if(cur->left) q.push(cur->left);
+            if(cur->right) q.push(cur->right);
         }
         return root;
+
     }
 };

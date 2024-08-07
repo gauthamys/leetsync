031a1f7e5488ccdd0d2f564352424285a6f2733b
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2024-08-07 14:09:27 +0530

    Time: 6 ms (88.20%) | Memory: 17 MB (10.18%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        set<pair<int, int>> s;
+        vector<pair<int, int>> s;
         for(int i=0; i<matrix.size(); i++) {
             for(int j=0; j<matrix[0].size(); j++) {
                 if(matrix[i][j] == 0){
-                    s.insert(make_pair(i, j));
+                    s.push_back(make_pair(i, j));
                 }
             }
         }

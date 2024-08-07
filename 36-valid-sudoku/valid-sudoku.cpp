17d192eaa03d7b19de0d1aaa8bcd96a1197955d4
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2024-08-07 12:52:33 +0530

    Time: 12 ms (85.18%) | Memory: 24.7 MB (39.22%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-       vector<set<int>> rows(9);
-       vector<set<int>> cols(9);
-       vector<set<int>> sub(9);
+       vector<set<char>> rows(9);
+       vector<set<char>> cols(9);
+       vector<set<char>> sub(9);
 
         for(int i=0; i<board.size(); i++) {
             for(int j=0; j<board[0].size(); j++) {
-                int cur = board[i][j];
-                if(cur == (int)'.') continue;
-                
+                char cur = board[i][j];
+                if(cur == '.') continue;
                 int subIndex = 3 * (i / 3) + j / 3;
 
                 if(rows[i].find(cur) != rows[i].end() || 

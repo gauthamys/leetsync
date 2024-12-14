3770095530cdc5a3022ed05794f22c0a6568461b
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2024-12-14 12:05:24 -0600

    Time: 0 ms (100.00%) | Memory: 8.8 MB (25.60%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for(auto c: s) {
+            if(c == '[' || c == '{' || c == '(') stk.push(c);
+            else {
+                if(stk.empty()) return false;
+                char top = stk.top();
+                if((c == '}' && top == '{') || (c == ']' && top == '[') || (c == ')' && top == '(')) stk.pop();
+                else return false;
+            }
+        }
+        return stk.empty();
+    }
+};

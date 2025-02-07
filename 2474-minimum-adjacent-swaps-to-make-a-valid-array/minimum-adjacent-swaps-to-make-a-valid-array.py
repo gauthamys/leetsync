0deecae9935d2d37ad05bf3bb0aab8ba7389771e
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2025-02-07 16:50:25 -0600

    Time: 6 ms (93.63%) | Memory: 31.3 MB (22.72%) - LeetSync

--- 2474-minimum-adjacent-swaps-to-make-a-valid-array/minimum-adjacent-swaps-to-make-a-valid-array.py
+++ 2474-minimum-adjacent-swaps-to-make-a-valid-array/minimum-adjacent-swaps-to-make-a-valid-array.py
@@ -1,22 +1,16 @@
 class Solution:
     def minimumSwaps(self, nums: List[int]) -> int:
         n = len(nums)
-        # Find the smallest and largest values in the array.
+
         min_val = min(nums)
         max_val = max(nums)
         
-        # Get the index of the first occurrence of the smallest element.
         pos_min = nums.index(min_val)
         
-        # Get the index of the last occurrence of the largest element.
-        # One way to do this is to reverse the list and find the index, then convert it.
         pos_max = n - 1 - nums[::-1].index(max_val)
         
-        # Compute the number of swaps:
         swaps = pos_min + ((n - 1) - pos_max)
         
-        # If the smallest element is to the right of the largest element,
-        # subtract 1 to account for the overlap.
         if pos_min > pos_max:
             swaps -= 1
             

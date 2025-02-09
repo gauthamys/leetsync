44198e32036d11e2c38b5acece00b556886c38df
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2025-02-09 17:22:50 -0600

    Time: 6 ms (78.29%) | Memory: 18 MB (74.53%) - LeetSync

---
+++ 605-can-place-flowers/can-place-flowers.py
@@ -0,0 +1,13 @@
+import math
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        flowerbed = [0] + flowerbed + [0]
+        maxFlowers = 0
+
+        for i in range(1, len(flowerbed) - 1):
+            if flowerbed[i] == 0 and flowerbed[i - 1] == 0 and flowerbed[i + 1] == 0:
+                maxFlowers += 1
+                flowerbed[i] = 1
+        
+        return maxFlowers >= n
+

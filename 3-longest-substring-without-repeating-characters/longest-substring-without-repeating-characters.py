aa51dd760446bdc784cc7abd1e20e2078a742045
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2025-02-09 00:54:36 -0600

    Time: 21 ms (38.00%) | Memory: 17.8 MB (47.90%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -6,7 +6,9 @@ class Solution:
         l, r = 0, 0
         while r < n:
             if s[r] in lookup:
-                l = max(lookup[s[r]], l)
+                for _ in range(l, lookup[s[r]]):
+                    del lookup[s[l]]
+                    l += 1
             
             res = max(res, r - l + 1)
             lookup[s[r]] = r + 1

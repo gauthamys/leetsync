f95760b72bf07a5f2a1653c474f00c7c8adde35e
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2025-02-09 13:50:35 -0600

    Time: 9 ms (21.62%) | Memory: 19.5 MB (21.86%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,25 +1,27 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         adj = {i:[] for i in range(numCourses)}
+        visited = set()
         for pre in prerequisites:
             adj[pre[0]].append(pre[1])
 
-        visited = set()
-        def dfs(cnum):
-            if adj[cnum] == []:
+        def dfs(cNum):
+            if adj[cNum] == []:
                 return True
-            if cnum in visited:
-                return False
-            visited.add(cnum)
-            for p in adj[cnum]:
-                if not dfs(p): 
-                    return False
 
-            visited.remove(cnum)
-            adj[cnum] = []
+            if cNum in visited:
+                return False
+
+            visited.add(cNum)
+            for p in adj[cNum]:
+                if not dfs(p):
+                    return False
+                    
+            visited.remove(cNum)
+            adj[cNum] = []
             return True
 
-        for course in adj.keys():
+        for course in adj:
             if not dfs(course):
                 return False
         

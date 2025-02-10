b831f90ca374a8bf1aae412e234733409ef46b7d
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2025-02-10 14:57:55 -0600

    Time: 1728 ms (5.00%) | Memory: 78.3 MB (60.40%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,53 +1,36 @@
-class ListNode:
+class Node:
     def __init__(self, key, val):
-        self.key = key
         self.val = val
-        self.prev = self.next = None
+        self.key = key
 
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.cache = {}
-        self.capacity = capacity
-        self.head = ListNode(-1, -1)
-        self.tail = ListNode(-1, -1)
-        self.head.next, self.tail.prev = self.tail, self.head
-
-    def add(self, node):
-        tmp = self.tail.prev
-        tmp.next = node
-        node.prev = tmp
-        node.next = self.tail
-        self.tail.prev = node
-
-
-    def remove(self, node):
-        node.prev.next, node.next.prev = node.next, node.prev
+        self.l = []
+        self.cap = capacity
 
     def get(self, key: int) -> int:
-        print('get', key)
-        if key in self.cache.keys():
-            self.remove(self.cache[key])
-            self.add(self.cache[key])
-            return self.cache[key].val
+        if key not in self.cache:
+            return -1
         
-        return -1
+        self.l.pop(self.l.index(self.cache[key]))
+        self.l.append(self.cache[key])
+        return self.cache[key].val
 
     def put(self, key: int, value: int) -> None:
-        print('put', key)
-        if key in self.cache.keys():
-            self.cache[key].val = value
-            self.remove(self.cache[key])
-            self.add(self.cache[key])
-            return
-        
-        if len(self.cache.keys()) == self.capacity:
-            del self.cache[self.head.next.key]
-            self.remove(self.head.next)
-
-        new = ListNode(key, value)
+        new = Node(key, value)
+        if key in self.cache:
+            self.l.pop(self.l.index(self.cache[key]))
+        else:
+            if self.cap == len(self.cache):
+                to_pop = self.l.pop(0)
+                del self.cache[to_pop.key]
+            
+        self.l.append(new)
         self.cache[key] = new
-        self.add(new)
+        
+
 
 
 # Your LRUCache object will be instantiated and called as such:

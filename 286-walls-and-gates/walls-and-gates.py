44318053b494a8f153cfeac521360adf1a1f1781
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2025-02-17 15:59:51 -0600

    Time: 175 ms (12.44%) | Memory: 25.7 MB (13.05%) - LeetSync

--- 286-walls-and-gates/walls-and-gates.py
+++ 286-walls-and-gates/walls-and-gates.py
@@ -3,21 +3,23 @@ class Solution:
         """
         Do not return anything, modify rooms in-place instead.
         """
-        MAX = 2 ** 31 - 1
         ROWS, COLS = len(rooms), len(rooms[0])
-        dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]]
         q = []
+        dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         for i in range(ROWS):
             for j in range(COLS):
                 if rooms[i][j] == 0:
-                    q.append((i, j))
-        
+                    q.append((i, j, 0))
+
+        visited = set()
         while q:
-            cur = q.pop(0)
-            for d in dirs:
-                r = cur[0] + d[0]
-                c = cur[1] + d[1]
-                if r < 0 or r >= ROWS or c < 0 or c >= COLS or rooms[r][c] != MAX:
-                    continue
-                q.append((r, c))
-                rooms[r][c] = rooms[cur[0]][cur[1]] + 1
+            qLen = len(q)
+            for _ in range(qLen):
+                cur = q.pop(0)
+                for d in dirs:
+                    r, c = cur[0] + d[0], cur[1] + d[1]
+                    if r < 0 or r > ROWS - 1 or c < 0 or c > COLS - 1 or rooms[r][c] == -1 or (r, c) in visited:
+                        continue
+                    rooms[r][c] = min(rooms[r][c], cur[2] + 1)
+                    q.append((r, c, cur[2] + 1))
+                    visited.add((r, c))

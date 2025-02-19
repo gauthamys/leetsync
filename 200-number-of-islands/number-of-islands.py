dceae6a913116b40d8935e90c887a52759408af7
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2025-02-18 23:32:21 -0600

    Time: 275 ms (29.66%) | Memory: 25.3 MB (27.99%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,19 +1,21 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
-        islands = 0
+        visited = set()
         dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        islands = 0
         for i in range(ROWS):
             for j in range(COLS):
-                if grid[i][j] == "1":
+                if grid[i][j] == "1" and (i , j) not in visited:
                     islands += 1
-                    stk = [(i, j)]
-                    while stk:
-                        cur = stk.pop()
+                    q = [(i, j)]
+                    visited.add((i, j))
+                    while q:
+                        cur = q.pop(0)
                         for d in dirs:
                             r, c = cur[0] + d[0], cur[1] + d[1]
-                            if r < 0 or r > ROWS - 1 or c < 0 or c > COLS - 1 or grid[r][c] != "1":
+                            if r < 0 or r > ROWS - 1 or c < 0 or c > COLS - 1 or grid[r][c] != "1" or (r, c) in visited:
                                 continue
-                            grid[r][c] = "0"
-                            stk.append((r, c))
-        return islands  
+                            q.append((r, c))
+                            visited.add((r, c))
+        return islands

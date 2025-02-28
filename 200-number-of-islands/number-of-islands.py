d71960a8b22add323cdb2ba5cf171493a3f123cb
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2025-02-28 14:43:45 -0600

    Time: 259 ms (45.02%) | Memory: 20.3 MB (44.72%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -2,19 +2,20 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
         dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        islands = 0
+        res = 0
         for i in range(ROWS):
             for j in range(COLS):
-                if grid[i][j] == "1":
-                    islands += 1
-                    q = [(i, j)]
-                    grid[i][j] = "0"
-                    while q:
-                        cur = q.pop(0)
-                        for d in dirs:
-                            r, c = cur[0] + d[0], cur[1] + d[1]
-                            if r < 0 or r > ROWS - 1 or c < 0 or c > COLS - 1 or grid[r][c] != "1":
-                                continue
-                            q.append((r, c))
-                            grid[r][c] = "0"
-        return islands
+                if grid[i][j] == "0":
+                    continue
+                stk = [(i, j)]
+                while stk:
+                    cur = stk.pop()
+                    for d in dirs:
+                        r, c = cur[0] + d[0], cur[1] + d[1]
+                        if r < 0 or r > ROWS - 1 or c < 0 or c > COLS - 1 or grid[r][c] != "1":
+                            continue
+                        grid[r][c] = "0"
+                        stk.append((r, c))
+                res += 1
+        
+        return res

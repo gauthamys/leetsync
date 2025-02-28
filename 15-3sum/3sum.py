256b32257ebc74f87bff361e427b727e96acbfbf
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2025-02-28 15:19:19 -0600

    Time: 590 ms (52.71%) | Memory: 20.5 MB (84.51%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -8,13 +8,13 @@ class Solution:
             l, r = i + 1, len(nums) - 1
             while l < r:
                 s = nums[i] + nums[l] + nums[r]
-                if s == 0:
-                    res.append([nums[i], nums[l], nums[r]])
-                    l += 1
-                    while nums[l] == nums[l - 1] and l < r:
-                        l += 1
-                elif s < 0:
+                if s < 0:
                     l += 1
                 elif s > 0:
                     r -= 1
-        return res 
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    while l < len(nums) and nums[l] == nums[l - 1]:
+                        l += 1
+        return res

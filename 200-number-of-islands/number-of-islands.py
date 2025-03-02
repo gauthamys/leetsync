2598aef94c17ea1b449ab4e2d98b0f8e183558f7
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2025-03-02 14:11:18 -0600

    Time: 246 ms (63.64%) | Memory: 20.3 MB (59.00%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -5,17 +5,15 @@ class Solution:
         res = 0
         for i in range(ROWS):
             for j in range(COLS):
-                if grid[i][j] == "0":
-                    continue
-                stk = [(i, j)]
-                while stk:
-                    cur = stk.pop()
-                    for d in dirs:
-                        r, c = cur[0] + d[0], cur[1] + d[1]
-                        if r < 0 or r > ROWS - 1 or c < 0 or c > COLS - 1 or grid[r][c] != "1":
-                            continue
-                        grid[r][c] = "0"
-                        stk.append((r, c))
-                res += 1
-        
+                if grid[i][j] == "1":
+                    stk = [(i, j)]
+                    while stk:
+                        cur = stk.pop()
+                        grid[cur[0]][cur[1]] = "0"
+                        for d in dirs:
+                            r, c = cur[0] + d[0], cur[1] + d[1]
+                            if r < 0 or r > ROWS - 1 or c < 0 or c > COLS - 1 or grid[r][c] != '1':
+                                continue
+                            stk.append((r, c))
+                    res += 1
         return res

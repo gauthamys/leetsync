b02fa94bd9f10f9adab137a7efbdabf14a4e6c75
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2025-03-03 11:34:15 -0600

    Time: 7 ms (49.34%) | Memory: 19.2 MB (52.94%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,22 +1,20 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        adj = {i:[] for i in range(numCourses)}
+        adj = defaultdict(list)
         indegree = [0] * numCourses
         for src, dest in prerequisites:
             adj[src].append(dest)
             indegree[dest] += 1
-        
-        q = [i for i in range(len(indegree)) if indegree[i] == 0]
 
+        q = [crs for crs in range(numCourses) if indegree[crs] == 0]
         finished = 0
+
         while q:
             cur = q.pop(0)
             for nei in adj[cur]:
                 indegree[nei] -= 1
                 if indegree[nei] == 0:
                     q.append(nei)
-            finished += 1
-        
+            finished += 1        
+
         return finished == numCourses
-        
-        

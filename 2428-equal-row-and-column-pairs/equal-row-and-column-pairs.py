bdfcb638f16c7c4a54d3a3f6b6cd9456c420a751
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2025-03-13 02:24:50 -0500

    Time: 68 ms (37.19%) | Memory: 22.9 MB (5.01%) - LeetSync

---
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.py
@@ -0,0 +1,20 @@
+class Solution:
+    def equalPairs(self, grid: List[List[int]]) -> int:
+        def make_key(l):
+            return '-'.join([str(i) for i in l])
+        
+        r = defaultdict(int)
+        c = defaultdict(int)
+        for i in range(len(grid)):
+            r[make_key(grid[i])] += 1
+        
+        for j in range(len(grid[0])):
+            col = [grid[r][j] for r in range(len(grid))]
+            c[make_key(col)] += 1
+        
+        res = 0
+        for l in r:
+            res += r[l] * c[l]
+        print(r, c)
+        return int(res)
+

b6f56ec4ff620d3febfc6627d57eddc0a7f8179a
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2025-03-29 22:29:55 -0500

    Time: 2 ms (86.68%) | Memory: 18 MB (6.83%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -2,24 +2,24 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
         q = []
+        dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         for i in range(ROWS):
             for j in range(COLS):
                 if grid[i][j] == 2:
                     q.append((i, j))
         
-        dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]]
         minutes = 0
         while q:
             qLen = len(q)
-            for i in range(qLen):
-                start = q.pop(0)
+            for _ in range(qLen):
+                cur = q.pop(0)
                 for d in dirs:
-                    r = start[0] + d[0] 
-                    c = start[1] + d[1]
-                    if r < 0 or r >= ROWS or c < 0 or c >= COLS or grid[r][c] != 1:
+                    r = cur[0] + d[0]
+                    c = cur[1] + d[1]
+                    if r > ROWS - 1 or r < 0 or c > COLS - 1 or c < 0 or grid[r][c] != 1:
                         continue
-                    grid[r][c] = 2
                     q.append((r, c))
+                    grid[r][c] = 2
             if q:
                 minutes += 1
         

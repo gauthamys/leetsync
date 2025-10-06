17cb560de1975a2a4c59329708ff09a66490116d
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2025-10-06 13:48:29 -0500

    Time: 2 ms (85.81%) | Memory: 17.6 MB (93.31%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -2,24 +2,27 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
         q = []
-        dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         for i in range(ROWS):
             for j in range(COLS):
                 if grid[i][j] == 2:
                     q.append((i, j))
-        
         minutes = 0
         while q:
             qLen = len(q)
             for _ in range(qLen):
-                cur = q.pop(0)
-                for d in dirs:
-                    r = cur[0] + d[0]
-                    c = cur[1] + d[1]
-                    if r > ROWS - 1 or r < 0 or c > COLS - 1 or c < 0 or grid[r][c] != 1:
-                        continue
-                    q.append((r, c))
-                    grid[r][c] = 2
+                curI, curJ = q.pop(0)
+                if curI + 1 < ROWS and grid[curI + 1][curJ] == 1:
+                    grid[curI + 1][curJ] = 2
+                    q.append((curI + 1, curJ))
+                if curI - 1 >= 0 and grid[curI - 1][curJ] == 1:
+                    grid[curI - 1][curJ] = 2
+                    q.append((curI - 1, curJ))
+                if curJ + 1 < COLS and grid[curI][curJ + 1] == 1:
+                    grid[curI][curJ + 1] = 2
+                    q.append((curI, curJ + 1))
+                if curJ - 1 >= 0 and grid[curI][curJ - 1] == 1:
+                    grid[curI][curJ - 1] = 2
+                    q.append((curI, curJ - 1))
             if q:
                 minutes += 1
         

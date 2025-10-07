eb4c0a63831d1604edb2fef0b7b0850edbe95ea6
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2025-10-07 00:56:02 -0500

    Time: 274 ms (25.82%) | Memory: 25.3 MB (29.82%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,20 +1,26 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        ROWS, COLS = len(grid), len(grid[0])
-        dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         islands = 0
+        visited = set()
+        ROWS, COLS = len(grid), len(grid[0])
         for i in range(ROWS):
             for j in range(COLS):
-                if grid[i][j] == "1":
-                    stk = [(i, j)]
-                    while stk:
-                        cur = stk.pop()
-                        grid[cur[0]][cur[1]] = "0"
-                        for d in dirs:
-                            r, c = cur[0] + d[0], cur[1] + d[1]
-                            if r < 0 or r > ROWS - 1 or c < 0 or c > COLS - 1 or grid[r][c] != "1":
-                                continue
-                            stk.append((r, c))
+                if grid[i][j] == "1" and (i, j) not in visited:
+                    q = [(i, j)]
+                    while q:
+                        curI, curJ = q.pop(0)
+                        if curI + 1 < ROWS and grid[curI + 1][curJ] == "1" and (curI + 1, curJ) not in visited:
+                            q.append((curI + 1, curJ))
+                            visited.add((curI + 1, curJ))
+                        if curI - 1 >= 0 and grid[curI - 1][curJ] == "1" and (curI - 1, curJ) not in visited:
+                            q.append((curI - 1, curJ))
+                            visited.add((curI - 1, curJ))
+                        if curJ + 1 < COLS and grid[curI][curJ + 1] == "1" and (curI, curJ + 1) not in visited:
+                            q.append((curI, curJ + 1))
+                            visited.add((curI, curJ + 1))
+                        if curJ - 1 >= 0 and grid[curI][curJ - 1] == "1" and (curI, curJ - 1) not in visited:
+                            q.append((curI, curJ - 1))
+                            visited.add((curI, curJ - 1))
                     islands += 1
         
         return islands

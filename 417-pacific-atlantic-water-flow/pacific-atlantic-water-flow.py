aefc9479d70ca6b2d64e18fd3dfe17d2c01a9da3
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2025-10-12 20:35:02 -0500

    Time: 27 ms (78.43%) | Memory: 19.4 MB (38.82%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -2,39 +2,36 @@ class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         ROWS, COLS = len(heights), len(heights[0])
         res = []
-        def bfs(r, c):
-            visited = set()
+        atl = set()
+        pac = set()
+        def bfs(r, c, visited):
             visited.add((r, c))
             queue = [(r, c)]
-            atl = False
-            pac = False
             while queue:
                 cr, cc = queue.pop(0)
-                if cr == ROWS - 1 or cc == COLS - 1:
-                    atl = True
-                if cr == 0 or cc == 0:
-                    pac = True
-                if pac == True and atl == True:
-                    return True
-
-                if cr + 1 < ROWS and heights[cr + 1][cc] <= heights[cr][cc] and (cr + 1, cc) not in visited:
+                if cr + 1 < ROWS and heights[cr + 1][cc] >= heights[cr][cc] and (cr + 1, cc) not in visited:
                     queue.append((cr + 1, cc))
                     visited.add((cr + 1, cc))
-                if cr - 1 >= 0 and heights[cr - 1][cc] <= heights[cr][cc] and (cr - 1, cc) not in visited:
+                if cr - 1 >= 0 and heights[cr - 1][cc] >= heights[cr][cc] and (cr - 1, cc) not in visited:
                     queue.append((cr - 1, cc))
                     visited.add((cr - 1, cc))
-                if cc + 1 < COLS and heights[cr][cc + 1] <= heights[cr][cc] and (cr, cc + 1) not in visited:
+                if cc + 1 < COLS and heights[cr][cc + 1] >= heights[cr][cc] and (cr, cc + 1) not in visited:
                     queue.append((cr, cc + 1))
                     visited.add((cr, cc + 1))
-                if cc - 1 >= 0 and heights[cr][cc - 1] <= heights[cr][cc] and (cr, cc - 1) not in visited:
+                if cc - 1 >= 0 and heights[cr][cc - 1] >= heights[cr][cc] and (cr, cc - 1) not in visited:
                     queue.append((cr, cc - 1))
                     visited.add((cr, cc - 1))
-            
-            return pac and atl
         
         for i in range(ROWS):
+            bfs(i, 0, pac)
+            bfs(i, COLS - 1, atl)
+        for j in range(COLS):
+            bfs(0, j, pac)
+            bfs(ROWS - 1, j, atl)
+
+        for i in range(ROWS):
             for j in range(COLS):
-                if bfs(i, j):
+                if (i, j) in atl and (i, j) in pac:
                     res.append((i, j))
         
         return res

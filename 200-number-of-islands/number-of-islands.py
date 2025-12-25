ab60500b05628871e7561d82bf4c12b0af125777
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2025-12-24 23:38:07 -0600

    Time: 242 ms (63.77%) | Memory: 24.8 MB (32.09%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,26 +1,28 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        islands = 0
         ROWS, COLS = len(grid), len(grid[0])
+        res = 0
+        visited = set()
         for i in range(ROWS):
             for j in range(COLS):
-                if grid[i][j] == "1" and (i, j):
-                    q = [(i, j)]
-                    while q:
-                        curI, curJ = q.pop(0)
-                        grid[curI][curJ] = "0"
-                        if curI + 1 < ROWS and grid[curI + 1][curJ] == "1" and (curI + 1, curJ):
-                            q.append((curI + 1, curJ))
-                            grid[curI + 1][curJ] = "0"
-                        if curI - 1 >= 0 and grid[curI - 1][curJ] == "1" and (curI - 1, curJ):
-                            q.append((curI - 1, curJ))
-                            grid[curI - 1][curJ] = "0"
-                        if curJ + 1 < COLS and grid[curI][curJ + 1] == "1" and (curI, curJ + 1):
-                            q.append((curI, curJ + 1))
-                            grid[curI][curJ + 1] = "0"
-                        if curJ - 1 >= 0 and grid[curI][curJ - 1] == "1" and (curI, curJ - 1):
-                            q.append((curI, curJ - 1))
-                            grid[curI][curJ - 1] = "0"
-                    islands += 1
-        
-        return islands
+                if grid[i][j] == "0" or (i, j) in visited:
+                    continue
+                q = [(i, j)]
+                visited.add((i, j))
+                while q:
+                    curI, curJ = q.pop(0)
+                    if curI + 1 < ROWS and grid[curI + 1][curJ] == "1" and (curI + 1, curJ) not in visited:
+                        q.append((curI + 1, curJ))
+                        visited.add((curI + 1, curJ))
+                    if curJ + 1 < COLS and grid[curI][curJ + 1] == "1" and (curI, curJ + 1) not in visited:
+                        q.append((curI, curJ + 1))
+                        visited.add((curI, curJ + 1))
+                    if curI - 1 >= 0 and grid[curI - 1][curJ] == "1" and (curI - 1, curJ) not in visited:
+                        q.append((curI - 1, curJ))
+                        visited.add((curI - 1, curJ))
+                    if curJ - 1 >= 0 and grid[curI][curJ - 1] == "1" and (curI, curJ - 1) not in visited:
+                        q.append((curI, curJ - 1))
+                        visited.add((curI, curJ - 1))
+                res += 1
+    
+        return res

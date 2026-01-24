ce5f0e8759f14e6e37edb2c979cdd0743ea62f7a
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-01-23 20:01:06 -0600

    Time: 5 ms (43.91%) | Memory: 19.4 MB (25.25%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,19 +1,18 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         ROWS, COLS = len(board), len(board[0])
-        rset = defaultdict(set)
-        cset = defaultdict(set)
-        subset = defaultdict(set)
+        r = defaultdict(set)
+        c = defaultdict(set)
+        s = defaultdict(set)
         for i in range(ROWS):
             for j in range(COLS):
                 if board[i][j] == '.':
                     continue
-                subIndex = 3 * (i // 3) + (j // 3)
-                if (board[i][j] in rset[i]) or (board[i][j] in cset[j]) or (board[i][j] in subset[subIndex]):
+                n = board[i][j]
+                sub_key = 3*(i//3) + j//3
+                if n in r[i] or n in c[j] or n in s[sub_key]:
                     return False
-
-                rset[i].add(board[i][j])
-                cset[j].add(board[i][j])
-                subset[subIndex].add(board[i][j])
-
+                r[i].add(n)
+                c[j].add(n)
+                s[sub_key].add(n)
         return True

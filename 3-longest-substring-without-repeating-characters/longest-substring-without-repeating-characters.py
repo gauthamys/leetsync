438651b35d02fc6215750cd9b7d3cac98dcdbfa8
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-01-25 16:50:14 -0600

    Time: 21 ms (29.38%) | Memory: 19.3 MB (25.19%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -4,14 +4,14 @@ class Solution:
         l, r = 0, 0
         res = 0
         while r < len(s):
-            if s[r] not in m:
-                m[s[r]] = r
-                r += 1
-            else:
+            if s[r] in m:
                 new_l = m[s[r]] + 1
-                for i in range(l, m[s[r]] + 1):
+                for i in range(l, new_l):
                     del m[s[i]]
                 l = new_l
-            res = max(res, (r - l))
+            else:
+                res = max(res, (r - l + 1))
+                m[s[r]] = r
+                r += 1
         
         return res

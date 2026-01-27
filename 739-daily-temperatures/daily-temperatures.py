9b05993912c0502da311d38f62eb22c5fa90f77a
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-01-27 11:24:37 -0600

    Time: 95 ms (61.37%) | Memory: 34.3 MB (15.84%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -3,13 +3,10 @@ class Solution:
         ans = [0] * len(temperatures)
         stk = []
         for i in range(len(temperatures)):
-            if not stk or temperatures[i] <= stk[-1][0]:
-                stk.append((temperatures[i], i))
-            else:
-                while stk and temperatures[i] > stk[-1][0]:
-                    top = stk.pop()
-                    ans[top[1]] = i - top[1]
-                stk.append((temperatures[i], i))
+            while stk and temperatures[i] > stk[-1][0]:
+                top = stk.pop()
+                ans[top[1]] = i - top[1]
+            stk.append((temperatures[i], i))
         
         return ans
 

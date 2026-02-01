f935df3bc25cbefb3684f45b33fcdbbb5f7adb55
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-02-01 17:12:31 -0600

    Time: 0 ms (100.00%) | Memory: 19.2 MB (44.41%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -5,25 +5,30 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        h1 = l1
-        h2 = l2
-        carry = 0
         res = ListNode(-1)
-        ret = res
-
-        while h1 or h2:
-            left = 0 if not h1 else h1.val
-            right = 0 if not h2 else h2.val
-            s = left + right + carry
-            ret.next = ListNode(s % 10)
-            ret = ret.next
-            carry = s // 10
-            if h1:
-                h1 = h1.next
-            if h2:
-                h2 = h2.next
-        
-        if carry:
-            ret.next = ListNode(carry)
-            
+        tmp = res
+        c = 0
+        while l1 and l2:
+            s = l1.val + l2.val + c
+            c = s // 10
+            tmp.next = ListNode(s % 10)
+            tmp = tmp.next
+            l1 = l1.next
+            l2 = l2.next
+        if l1:
+            while l1:
+                s = l1.val + c
+                tmp.next = ListNode(s % 10)
+                c = s // 10
+                tmp = tmp.next
+                l1 = l1.next
+        if l2:
+            while l2:
+                s = l2.val + c
+                tmp.next = ListNode(s % 10)
+                c = s // 10
+                tmp = tmp.next
+                l2 = l2.next
+        if c:
+            tmp.next = ListNode(c)
         return res.next

e910e1dc1dfd56a770d209eac08df8f8cce36a8e
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-02-03 14:03:57 -0600

    Time: 8 ms (7.34%) | Memory: 19.4 MB (39.83%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,34 +1,33 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
+        visited = set()
+        dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        minutes = 0
         q = []
+        
         for i in range(ROWS):
             for j in range(COLS):
                 if grid[i][j] == 2:
                     q.append((i, j))
-        minutes = 0
+                    visited.add((i, j))
+        
         while q:
             qLen = len(q)
             for _ in range(qLen):
                 curI, curJ = q.pop(0)
-                if curI + 1 < ROWS and grid[curI + 1][curJ] == 1:
-                    grid[curI + 1][curJ] = 2
-                    q.append((curI + 1, curJ))
-                if curI - 1 >= 0 and grid[curI - 1][curJ] == 1:
-                    grid[curI - 1][curJ] = 2
-                    q.append((curI - 1, curJ))
-                if curJ + 1 < COLS and grid[curI][curJ + 1] == 1:
-                    grid[curI][curJ + 1] = 2
-                    q.append((curI, curJ + 1))
-                if curJ - 1 >= 0 and grid[curI][curJ - 1] == 1:
-                    grid[curI][curJ - 1] = 2
-                    q.append((curI, curJ - 1))
+                for dI, dJ in dirs:
+                    newI = curI + dI
+                    newJ = curJ + dJ
+                    if newI < ROWS and newI >= 0 and newJ < COLS and newJ>= 0 and grid[newI][newJ] == 1 and (newI, newJ) not in visited:
+                        q.append((newI, newJ))
+                        visited.add((newI, newJ))
             if q:
                 minutes += 1
         
         for i in range(ROWS):
             for j in range(COLS):
-                if grid[i][j] == 1:
+                if grid[i][j] == 1 and (i, j) not in visited:
                     return -1
         
         return minutes

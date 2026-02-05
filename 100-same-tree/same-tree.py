aeebea8084aedd83273483c8fc9f86fca26c4ddc
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-02-05 00:16:07 -0600

    Time: 0 ms (100.00%) | Memory: 19.4 MB (39.45%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -6,30 +6,12 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p and not q:
-            return True
-        if p and not q or q and not p:
-            return False
-        stk1 = [p]
-        stk2 = [q]
-        while stk1 or stk2:
-            cur1 = stk1.pop()
-            cur2 = stk2.pop()
-            if cur1.val != cur2.val:
+        def same(node1, node2):
+            if not node1 and not node2:
+                return True
+            if node1 and not node2 or node2 and not node1:
                 return False
-            if cur1.left and cur2.left:
-                stk1.append(cur1.left)
-                stk2.append(cur2.left)
-            if cur1.right and cur2.right:
-                stk1.append(cur1.right)
-                stk2.append(cur2.right)
-            if cur1.left and not cur2.left:
+            if node1.val != node2.val:
                 return False
-            if cur1.right and not cur2.right:
-                return False
-            if cur2.left and not cur1.left:
-                return False
-            if cur2.right and not cur1.right:
-                return False
-
-        return True
+            return same(node1.left, node2.left) and same(node1.right, node2.right)
+        return same(p, q)

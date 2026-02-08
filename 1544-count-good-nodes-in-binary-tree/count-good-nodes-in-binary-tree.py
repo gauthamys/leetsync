32b22eae2cc829fe60d8282b30459ffbaafe7c5e
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-02-08 02:11:45 -0600

    Time: 133 ms (56.34%) | Memory: 32.3 MB (10.31%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -6,14 +6,14 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        stk = [[root, root.val]]
+        stk = [(root, root.val)]
         res = 0
         while stk:
-            cur_node, cur_max = stk.pop()
-            if cur_node.val >= cur_max:
+            cur, cur_max = stk.pop()
+            if cur.left:
+                stk.append((cur.left, max(cur_max, cur.val)))
+            if cur.right:
+                stk.append((cur.right, max(cur_max, cur.val)))
+            if cur.val >= cur_max:
                 res += 1
-            if cur_node.left:
-                stk.append([cur_node.left, max(cur_node.val, cur_max)])
-            if cur_node.right:
-                stk.append([cur_node.right, max(cur_node.val, cur_max)])
         return res

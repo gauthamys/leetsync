36fa7694ec572ac223078a79b8238aadf82c8d50
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-02-12 11:22:46 -0600

    Time: 3 ms (34.18%) | Memory: 20.9 MB (46.55%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -6,13 +6,13 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        q = [[root, float('inf'), float('-inf')]]
+        q = [(root, float('inf'), float('-inf'))]
         while q:
-            cur, left_max, right_min = q.pop(0)
-            if cur.val <= right_min or cur.val >= left_max:
+            cur, leftMax, rightMin = q.pop(0)
+            if cur.val >= leftMax or cur.val <= rightMin:
                 return False
             if cur.left:
-                q.append([cur.left, min(left_max, cur.val), right_min])
+                q.append([cur.left, min(leftMax, cur.val), rightMin])
             if cur.right:
-                q.append([cur.right, left_max, max(right_min, cur.val)])
+                q.append([cur.right, leftMax, max(rightMin, cur.val)])
         return True

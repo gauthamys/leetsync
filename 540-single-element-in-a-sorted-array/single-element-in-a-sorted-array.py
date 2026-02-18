8b36d2cbe7b6813066e747eab5b88ee7b9189e46
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-02-17 20:22:24 -0600

    Time: 0 ms (100.00%) | Memory: 27 MB (42.64%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -3,14 +3,15 @@ class Solution:
         l, r = 0, len(nums) - 1
         while l < r:
             mid = (l + r) // 2
-            flag = (r - mid) % 2 == 0
             if nums[mid] == nums[mid + 1]:
-                if flag:
+                # remove (mid, mid + 1)
+                # if left is odd go there, if right is odd go there
+                if (r - mid - 1) % 2 == 1:
                     l = mid + 2
                 else:
                     r = mid - 1
             elif nums[mid] == nums[mid - 1]:
-                if flag:
+                if (mid - 1 - l) % 2 == 1:
                     r = mid - 2
                 else:
                     l = mid + 1

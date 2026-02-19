59069da4a972b3021413b89d4c6935ed33657e3e
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-02-19 13:39:27 -0600

    Time: 7 ms (32.00%) | Memory: 19.6 MB (7.81%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,18 +1,20 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        ROWS, COLS = len(board), len(board[0])
-        r = defaultdict(set)
-        c = defaultdict(set)
-        s = defaultdict(set)
-        for i in range(ROWS):
-            for j in range(COLS):
+        rows = {i:set() for i in range(10)}
+        cols = {i: set() for i in range(10)}
+        sub = {i: set() for i in range(10)}
+
+        m, n = len(board), len(board[0])
+        for i in range(m):
+            for j in range(n):
                 if board[i][j] == '.':
                     continue
-                n = board[i][j]
-                sub_key = 3*(i//3) + j//3
-                if n in r[i] or n in c[j] or n in s[sub_key]:
+                subIndex = (3 * (j // 3)) + (i // 3)
+                print(subIndex)
+                if board[i][j] in rows[i] or board[i][j] in cols[j] or board[i][j] in sub[subIndex]:
                     return False
-                r[i].add(n)
-                c[j].add(n)
-                s[sub_key].add(n)
+                rows[i].add(board[i][j])
+                cols[j].add(board[i][j])
+                sub[subIndex].add(board[i][j])
         return True
+                

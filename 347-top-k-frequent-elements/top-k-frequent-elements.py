4448cb1cd22616f855821f09c419869d02482686
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-02-19 13:20:04 -0600

    Time: 4 ms (94.96%) | Memory: 14.4 MB (62.33%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,9 +1,15 @@
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        m = {}
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        counts = {}
         for n in nums:
-            if n in m:
-                m[n] += 1
+            if n not in counts:
+                counts[n] = 1
             else:
-                m[n] = 0
-        return [x[0] for x in sorted(m.items(), key=lambda y: -y[1])][:k]
+                counts[n] += 1
+        return [x[0] for x in sorted(counts.items(), key=lambda y: y[1],reverse=True)][:k]
+        

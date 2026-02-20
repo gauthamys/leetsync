f308ab51cbaa067745b6b4ea66ca39d312e09b12
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-02-20 13:28:38 -0600

    Time: 108 ms (41.55%) | Memory: 35 MB (15.67%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,14 +1,11 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        stk = []
+        stk = [] # pair (t, i)
         res = [0] * len(temperatures)
-        for i, t in enumerate(temperatures):
-            #print(stk)
-            if not stk or stk[-1][0] >= t:
-                stk.append((t, i))
-            elif t > stk[-1][0]:
-                while stk and stk[-1][0] < t:
-                    curT, curI = stk.pop()
-                    res[curI] = i - curI
-                stk.append((t, i))
+        for i in range(len(temperatures)):
+            days = 0
+            while stk and stk[-1][0] < temperatures[i]:
+                t, index = stk.pop()
+                res[index] = i - index
+            stk.append((temperatures[i], i))
         return res

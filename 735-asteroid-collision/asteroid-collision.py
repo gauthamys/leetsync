7df8130b23013fffb081348cf3ac0acf675b4991
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-02-20 01:51:57 -0600

    Time: 11 ms (16.41%) | Memory: 20.4 MB (27.83%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.py
@@ -0,0 +1,20 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stk = []
+        for a in asteroids:
+            #print(stk)
+            if not stk or stk[-1] / abs(stk[-1]) == a / abs(a):
+                stk.append(a)
+            else:
+                explode = False
+                new = a
+                while stk and new < 0 and stk[-1] > 0:
+                    top = stk.pop()
+                    if top + a == 0:
+                        explode = True
+                        break
+                    new = top if abs(top) > abs(new) else new
+                if not explode:
+                    stk.append(new)
+        return stk
+                

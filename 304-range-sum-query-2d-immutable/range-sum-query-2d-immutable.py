5c9ab0b05c440a56aa4b20bf652f411591ae73d2
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-02-19 19:37:37 -0600

    Time: 103 ms (80.83%) | Memory: 33.7 MB (42.28%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
@@ -1,22 +1,20 @@
 class NumMatrix:
-
     def __init__(self, matrix: List[List[int]]):
-        m, n = len(matrix), len(matrix[0])
-        self.prefix = [[0] * n for _ in range(m)]
-        for i in range(m):
-            cur = 0
-            for j in range(n):
-                cur += matrix[i][j]
-                self.prefix[i][j] = cur
+        m = len(matrix)
+        n = len(matrix[0])
+        self.dp = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                self.dp[i][j] = matrix[i - 1][j - 1] + \
+                 self.dp[i - 1][j] + \
+                 self.dp[i][j - 1] - \
+                 self.dp[i - 1][j - 1]
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        res = 0
-        for i in range(row1, row2 + 1):
-            if col1 == 0:
-                res += self.prefix[i][col2]
-            else: 
-                res += self.prefix[i][col2] - self.prefix[i][col1 - 1]
-        return res
+        return self.dp[row2 + 1][col2 + 1] - \
+        self.dp[row1][col2 + 1] - \
+        self.dp[row2 + 1][col1] + \
+        self.dp[row1][col1]
 
 
 # Your NumMatrix object will be instantiated and called as such:

98b3f458dbe87555bf2002f66ac4a792d5d6777f
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-02-20 23:12:53 -0600

    Time: 47 ms (20.73%) | Memory: 21.8 MB (11.59%) - LeetSync

---
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -0,0 +1,6 @@
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        ar = [(abs(arr[i] - x), arr[i]) for i in range(len(arr))]
+        ar.sort(key=lambda j: j[0])
+        print(ar)
+        return list(sorted([i[1] for i in ar[:k]]))

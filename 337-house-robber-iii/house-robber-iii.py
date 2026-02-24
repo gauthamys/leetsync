04d796b3677daccf708774ec787742a655f6bf6f
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-02-23 19:32:57 -0600

    Time: 0 ms (100.00%) | Memory: 20.9 MB (61.52%) - LeetSync

--- 337-house-robber-iii/house-robber-iii.py
+++ 337-house-robber-iii/house-robber-iii.py
@@ -8,12 +8,12 @@ class Solution:
     def rob(self, root: Optional[TreeNode]) -> int:
         def dfs(node):
             if not node:
-                return (0, 0) # rob, not rob
+                return 0, 0
             left = dfs(node.left)
             right = dfs(node.right)
             rob = node.val + left[1] + right[1]
             not_rob = max(left) + max(right)
-            return (rob, not_rob)
+            return rob, not_rob
         return max(dfs(root))
 
             

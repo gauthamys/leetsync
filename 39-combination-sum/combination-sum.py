ca5fbff68cff7e7ad72ba13e10eb642c4ab1c9e8
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-02-23 23:44:30 -0600

    Time: 23 ms (11.03%) | Memory: 19.6 MB (54.28%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -2,17 +2,14 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
         visited = set()
-        def dfs(cur):
+        def dfs(i, cur):
             if sum(cur) == target:
-                if tuple(sorted(cur)) not in visited:
-                    res.append(cur)
-                    visited.add(tuple(sorted(cur)))
-                return
-            if sum(cur) > target:
+                res.append(cur)
+            if i >= len(candidates) or sum(cur) > target:
                 return
             else:
-                for j in range(len(candidates)):
-                    dfs(cur + [candidates[j]])
-        dfs([])
+                for j in range(i, len(candidates)):
+                    dfs(j, cur + [candidates[j]])
+        dfs(0, [])
         return res
             

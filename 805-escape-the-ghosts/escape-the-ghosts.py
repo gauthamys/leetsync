2a18b65bc98ec072880b0604e73e4a62d30fb99c
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-02-23 20:01:51 -0600

    Time: 0 ms (100.00%) | Memory: 19.3 MB (87.11%) - LeetSync

--- 805-escape-the-ghosts/escape-the-ghosts.py
+++ 805-escape-the-ghosts/escape-the-ghosts.py
@@ -1,7 +1,9 @@
-class Solution(object):
-    def escapeGhosts(self, ghosts, target):
-        def taxi(P, Q):
-            return abs(P[0] - Q[0]) + abs(P[1] - Q[1])
-
-        return all(taxi([0, 0], target) < taxi(ghost, target)
-                   for ghost in ghosts)
+class Solution:
+    def escapeGhosts(self, ghosts: List[List[int]], target: List[int]) -> bool:
+        def dist(point):
+            return abs(point[0] - target[0]) + abs(point[1] - target[1])
+        DIST = abs(target[0]) + abs(target[1])
+        for point in ghosts:
+            if dist(point) <= DIST:
+                return False
+        return True

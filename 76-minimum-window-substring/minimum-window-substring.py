ad0ad3dee6fadd076e930c90c0a9c43b82fdffe4
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-02-25 17:03:47 -0600

    Time: 593 ms (6.35%) | Memory: 19.8 MB (33.81%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        need = {}
+        for ch in t:
+            need[ch] = need.get(ch, 0) + 1
+        
+        res_r, res_l = float('inf'), 0
+        l, r = 0, 0
+        have = {}
+
+        def check():
+            for ch in need:
+                if ch not in have or have[ch] < need[ch]:
+                    return False
+            return True
+
+        while r < len(s):
+            have[s[r]] = have.get(s[r], 0) + 1
+            if check():
+                while check():
+                    if (r - l) < (res_r - res_l):
+                        res_r, res_l = r, l
+                    have[s[l]] -= 1
+                    l += 1
+            r += 1
+        
+        return s[res_l:res_r+1] if res_r != float('inf') else ""

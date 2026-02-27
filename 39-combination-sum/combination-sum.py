ae87443eac3343fca63fda9a9c6d524215f066f6
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-02-27 16:51:12 -0600

    Time: 7 ms (76.51%) | Memory: 19.7 MB (58.38%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -2,6 +2,7 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
         cur = []
+        
         def backtrack(start, curSum):
             if curSum == target:
                 res.append(cur[:])
@@ -9,13 +10,9 @@ class Solution:
             if curSum > target:
                 return
             for i in range(start, len(candidates)):
-                if i > start and candidates[i] == candidates[i - 1]:
-                    continue
                 cur.append(candidates[i])
                 backtrack(i, curSum + candidates[i])
                 cur.pop()
         
         backtrack(0, 0)
         return res
-            
-            

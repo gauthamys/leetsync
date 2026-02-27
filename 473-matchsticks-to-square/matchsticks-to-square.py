69ad5fa606f8dae5b3f0befaa4fb9f8183fa3bd1
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-02-27 17:02:07 -0600

    Time: 4456 ms (10.28%) | Memory: 19.4 MB (52.18%) - LeetSync

--- 473-matchsticks-to-square/matchsticks-to-square.py
+++ 473-matchsticks-to-square/matchsticks-to-square.py
@@ -7,26 +7,27 @@ class Solution:
         if total % 4 != 0:
             return False
         
-        matchsticks.sort(reverse=True)
         side = total // 4
-        sides = [0, 0, 0, 0]
-        def backtrack(start):
-            if start == len(matchsticks):
-                return sides[0] == sides[1] == sides[2] == sides[3] == side
-            
-            x = matchsticks[start]
-            for i in range(4):
-                if sides[i] + x <= side:
-                    sides[i] += x
-                    if backtrack(start + 1):
-                        return True
-                    sides[i] -= x
+        sides = [0] * 4
+        matchsticks.sort(reverse=True)
+        def backtrack(idx):
+            if sides[0] == sides[1] == sides[2] == sides[3] == side:
+                return True
 
-                if sides[i] == 0:
-                    break
+            if idx >= len(matchsticks):
+                return False
+
+            for i in range(4):
+                if sides[i] + matchsticks[idx] <= side:
+                    sides[i] += matchsticks[idx]
+                    if backtrack(idx + 1):
+                        return True
+                    sides[i] -= matchsticks[idx]
             
-            return False
+            if sides[i] == 0:
+                return False
         
         return backtrack(0)
-                
-            
+        
+        
+        

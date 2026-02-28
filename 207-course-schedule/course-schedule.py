ec454a9e6d658ef11c21dd6f81bad382cc9da16d
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-02-28 00:10:48 -0600

    Time: 1 ms (91.92%) | Memory: 20.4 MB (73.05%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,20 +1,21 @@
+from collections import deque
+
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        adj = {x:[] for x in range(numCourses)}
-        indegree = [0] * numCourses
-        for src, dest in prerequisites:
-            adj[src].append(dest)
-            indegree[dest] += 1
+        adj = {i: [] for i in range(numCourses)}
+        degree = [0] * numCourses
+        for a, b in prerequisites:
+            adj[b].append(a)
+            degree[a] += 1
         
-        q = [crs for crs in range(numCourses) if indegree[crs] == 0]
+        q = deque([node for node in adj if degree[node] == 0])
         finished = 0
         while q:
-            cur = q.pop(0)
+            cur = q.popleft()
             for nei in adj[cur]:
-                indegree[nei] -= 1
-                if indegree[nei] == 0:
+                degree[nei] -= 1
+                if degree[nei] == 0:
                     q.append(nei)
             finished += 1
         
         return finished == numCourses
-                

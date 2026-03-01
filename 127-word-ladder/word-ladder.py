ac644192f1b16a4317f41fee1153eb7b58d9820c
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-03-01 11:56:05 -0600

    Time: 217 ms (54.59%) | Memory: 22.5 MB (38.63%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -1,40 +1,38 @@
-from collections import defaultdict, deque
-
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         if endWord not in wordList:
             return 0
         
         def pattern(word):
-            patterns = []
-            for j in range(len(word)):
-                patterns.append(word[:j] + "*" + word[j + 1:])
-            return patterns
-
+            res = []
+            for i in range(len(word)):
+                res.append(word[:i] + '*' + word[i + 1:])
+            return res
+        
         adj = defaultdict(list)
         wordList.append(beginWord)
-        for w in wordList:
-            for pat in pattern(w):
-                adj[pat].append(w)
-
+        for word in wordList:
+            for pat in pattern(word):
+                adj[pat].append(word)
+        
         q = deque([beginWord])
         visited = set()
         visited.add(beginWord)
         res = 1
-
+        
         while q:
             qLen = len(q)
             for _ in range(qLen):
-                cur = q.popleft()
-                if cur == endWord:
+                curWord = q.popleft()
+                if curWord == endWord:
                     return res
-                
-                for pat in pattern(cur):
+
+                for pat in pattern(curWord):
                     for nei in adj[pat]:
-                        if nei in visited:
+                        if nei in visited or curWord not in wordList:
                             continue
                         q.append(nei)
                         visited.add(nei)
             res += 1
-        
+
         return 0

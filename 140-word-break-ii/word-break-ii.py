04588f9e2cdeabe1426452a39f4923ca081e5b67
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-03-01 00:50:29 -0600

    Time: 0 ms (100.00%) | Memory: 19.4 MB (74.20%) - LeetSync

---
+++ 140-word-break-ii/word-break-ii.py
@@ -0,0 +1,25 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        res = []
+        wordDict = set(wordDict)
+
+        def backtrack(start, curWord, curSentence):
+            word = ''.join(curWord)
+            if start >= len(s):
+                if word in wordDict:
+                    curSentence.append(word)
+                    res.append(" ".join(curSentence))
+                    curSentence.pop()
+                return
+            
+            if word in wordDict:
+                curSentence.append(word)
+                backtrack(start + 1, [s[start]], curSentence)
+                curSentence.pop()
+            
+            curWord.append(s[start])
+            backtrack(start + 1, curWord, curSentence)
+            curWord.pop()
+        
+        backtrack(0, [], [])
+        return res

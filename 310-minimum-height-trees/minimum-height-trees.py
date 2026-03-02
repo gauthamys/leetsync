cc42c14fad0b35a4e65c992a309b4693397cdbbf
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-03-02 11:18:34 -0600

    Time: 55 ms (49.37%) | Memory: 28 MB (63.36%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.py
+++ 310-minimum-height-trees/minimum-height-trees.py
@@ -1,12 +1,10 @@
-from collections import deque
-
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
         if n <= 2:
             return list(range(n))
         
+        adj = {i:[] for i in range(n)}
         degree = [0] * n
-        adj = {i: [] for i in range(n)}
         for a, b in edges:
             adj[a].append(b)
             adj[b].append(a)
@@ -14,12 +12,15 @@ class Solution:
             degree[a] += 1
         
         q = deque([node for node in adj if degree[node] == 1])
+
         while q:
             if n <= 2:
                 return list(q)
+            
             for _ in range(len(q)):
                 cur = q.popleft()
                 n -= 1
+
                 for nei in adj[cur]:
                     degree[nei] -= 1
                     if degree[nei] == 1:

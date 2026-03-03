080e664eaaba8014b2483dcbf732a659cca56998
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-03-03 15:00:44 -0600

    Time: 0 ms (100.00%) | Memory: 22.4 MB (71.43%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -3,39 +3,54 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        rows, cols = len(board), len(board[0])
         visited = set()
+        rows, cols = len(board), len(board[0])
+        q = deque([])
+        
+        # left and right
+        for i in range(rows):
+            if board[i][0] == 'O':
+                q.append((i, 0))
+                visited.add((i, 0))
+            
+            if board[i][cols - 1] == 'O':
+                q.append((i, cols - 1))
+                visited.add((i, cols - 1))
+        
+        # top and bottom
+        for j in range(cols):
+            if board[0][j] == 'O':
+                q.append((0, j))
+                visited.add((0, j))
+            
+            if board[rows - 1][j] == 'O':
+                q.append((rows - 1, j))
+                visited.add((rows - 1, j))
+        
+        while q:
+            qLen = len(q)
+            for _ in range(qLen):
+                cur_i, cur_j = q.popleft()
+                if cur_i + 1 < rows and board[cur_i + 1][cur_j] == 'O' and (cur_i + 1, cur_j) not in visited:
+                    q.append((cur_i + 1, cur_j))
+                    visited.add((cur_i + 1, cur_j))
+                
+                if cur_i - 1 >= 0 and board[cur_i - 1][cur_j] == 'O' and (cur_i - 1, cur_j) not in visited:
+                    q.append((cur_i - 1, cur_j))
+                    visited.add((cur_i - 1, cur_j))
+                
+                if cur_j + 1 < cols and board[cur_i][cur_j + 1] == 'O' and (cur_i, cur_j + 1) not in visited:
+                    q.append((cur_i, cur_j + 1))
+                    visited.add((cur_i, cur_j + 1))
+                
+                if cur_j - 1 >= 0 and board[cur_i][cur_j - 1] == 'O' and (cur_i, cur_j - 1) not in visited:
+                    q.append((cur_i, cur_j - 1))
+                    visited.add((cur_i, cur_j - 1))
         
         for i in range(rows):
             for j in range(cols):
-                if board[i][j] != 'O' or (i, j) in visited:
-                    continue
-                
-                visited.add((i, j))
-                q = deque([(i, j)])
-                path = []
-                surrounded = True
+                if board[i][j] == 'O' and (i, j) not in visited:
+                    board[i][j] = 'X'
 
-                while q:
-                    cur_i, cur_j = q.popleft()
-                    if cur_i in [0, rows - 1] or cur_j in [0, cols - 1]:
-                        surrounded = False
-                    
-                    path.append((cur_i, cur_j))
-                    children = [
-                        (cur_i + 1, cur_j),
-                        (cur_i - 1, cur_j),
-                        (cur_i, cur_j + 1),
-                        (cur_i, cur_j - 1)
-                    ]
-                    
-                    for ci, cj in children:
-                        if ci < 0 or ci >= rows or cj < 0 or cj >= cols or (ci, cj) in visited or board[ci][cj] != 'O':
-                            continue
-                        
-                        visited.add((ci, cj))
-                        q.append((ci, cj))
-                
-                if surrounded:
-                    for x_i, x_j in path:
-                        board[x_i][x_j] = 'X'
+
+        

02cc337d5755e7081659a0876501889021c29b22
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-03-03 14:28:46 -0600

    Time: 231 ms (13.16%) | Memory: 25.8 MB (76.40%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -0,0 +1,20 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        # length of largest subarray with 2 distinct elements
+        l, r = 0, 0
+        m = {}
+        res = float('-inf')
+        while r < len(fruits):
+            m[fruits[r]] = m.get(fruits[r], 0) + 1
+            r += 1
+
+            while l < len(fruits) and len(m) > 2:
+                m[fruits[l]] -= 1
+                if m[fruits[l]] == 0:
+                    del m[fruits[l]]
+                l += 1
+            
+            res = max(res, (r - l))
+        
+        return res
+            

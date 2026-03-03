0bc1e0a20168ed62047fd85174c9b4cd06ca5547
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-03-03 14:41:09 -0600

    Time: 11 ms (58.49%) | Memory: 19.2 MB (93.26%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.py
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -1,23 +1,22 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        d = "DOWN"
-        arrs = [[] for _ in range(numRows)]
-        i = 0
+        rows = [[] for _ in range(numRows)]
+        d = 'DOWN'
+        cur = 0
         for c in s:
-            arrs[i].append(c)
-            if d == "DOWN":
-                if i < numRows - 1:
-                    i += 1
+            rows[cur].append(c)
+            if d == 'DOWN':
+                if cur < numRows - 1:
+                    cur += 1
                 else:
-                    d = "UP"
-                    i -= 1
-            elif d == "UP":
-                if i > 0:
-                    i -= 1
+                    d = 'UP'
+                    cur -= 1
+            else:
+                if cur > 0:
+                    cur -= 1
                 else:
                     d = "DOWN"
-                    i += 1
-        return ''.join([''.join(arrs[i]) for i in range(numRows)])
+                    cur += 1
+        return ''.join(''.join(x) for x in rows)
                 
-                
-                
+            

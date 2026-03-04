01481b8d55dd8e509d5e7708b2542d456c1abe00
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-03-03 23:35:26 -0600

    Time: 0 ms (100.00%) | Memory: 19.1 MB (88.91%) - LeetSync

---
+++ 249-group-shifted-strings/group-shifted-strings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupStrings(self, strings: List[str]) -> List[List[str]]:
+        m = {}
+        
+        for s in strings:
+            key = ''
+            for i in range(1, len(s)):
+                key += str((ord(s[i]) - ord(s[0])) % 26 + ord('a'))
+            m[key] = m.get(key, [])
+            m[key].append(s)
+        
+        return list(m.values())

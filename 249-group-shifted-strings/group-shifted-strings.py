c1f56fe809b9252572ce1939ee40da3c20fc2181
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-03-03 23:36:11 -0600

    Time: 1 ms (56.14%) | Memory: 19.4 MB (72.24%) - LeetSync

--- 249-group-shifted-strings/group-shifted-strings.py
+++ 249-group-shifted-strings/group-shifted-strings.py
@@ -4,8 +4,8 @@ class Solution:
         
         for s in strings:
             key = ''
-            for i in range(1, len(s)):
-                key += str((ord(s[i]) - ord(s[i - 1])) % 26 + ord('a'))
+            for i in range(len(s)):
+                key += str((ord(s[i]) - ord(s[0])) % 26 + ord('a'))
             m[key] = m.get(key, [])
             m[key].append(s)
         

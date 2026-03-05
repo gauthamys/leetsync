978d7222372bc596b4e3479515f317e41eaeaad4
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-03-05 17:24:30 -0600

    Time: 35 ms (51.96%) | Memory: 22.7 MB (53.43%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -1,13 +1,11 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        prefix = [0] * len(nums)
         pre = 0
         counts = {0: 1}
         res = 0
 
         for i in range(len(nums)):
             pre += nums[i]
-            prefix[i] = pre
             if pre - goal in counts:
                 res += counts[pre - goal]
             counts[pre] = counts.get(pre, 0) + 1

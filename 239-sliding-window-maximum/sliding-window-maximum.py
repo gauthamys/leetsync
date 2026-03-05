52a3e218ba543f6e925c303844ab719c0f4b56d0
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-03-05 02:37:54 -0600

    Time: 155 ms (94.51%) | Memory: 35.5 MB (32.89%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -1,7 +1,8 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        dq = deque([])
+        dq = deque([]) # idx
         res = []
+        
         for i in range(k):
             while dq and nums[dq[-1]] <= nums[i]:
                 dq.pop()
@@ -10,16 +11,14 @@ class Solution:
         res.append(nums[dq[0]])
         
         for i in range(k, len(nums)):
-            if dq and dq[0] == i - k:
+            if i - dq[0] == k:
                 dq.popleft()
             
-            while dq and nums[i] >= nums[dq[-1]]:
+            while dq and nums[dq[-1]] <= nums[i]:
                 dq.pop()
             
             dq.append(i)
             res.append(nums[dq[0]])
         
         return res
-    
-    # 1 3 -1 -3 5 3 6 7
-    # dq = [3, -1]
+            

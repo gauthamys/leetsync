e1ebfd6ce22dce0fb1f4bcc28acd4d37bf2d7240
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-03-06 13:38:38 -0600

    Time: 79 ms (66.99%) | Memory: 20.9 MB (37.75%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -4,29 +4,32 @@ class Solution:
         for task in tasks:
             counts[task] += 1
         
-        pq = [(-counts[v], v) for v in counts]
-        heapq.heapify(pq)
+        maxHeap = [-counts[t] for t in counts]
+        heapq.heapify(maxHeap)
         time = 0
 
-        while pq:
+        while maxHeap:
             cycle = n + 1
             store = []
             task_count = 0
-            while cycle > 0 and pq:
-                current_freq, current_task = heapq.heappop(pq)
-                current_freq = -current_freq
+            while cycle > 0 and maxHeap:
+                current_freq = -heapq.heappop(maxHeap)
                 if current_freq > 1:
-                    store.append((-(current_freq - 1), current_task))
-                task_count += 1
+                    store.append(-(current_freq - 1))
                 cycle -= 1
-            
+                task_count += 1
             for x in store:
-                heapq.heappush(pq, x)
-            
-            time += task_count if not pq else n + 1
+                heapq.heappush(maxHeap, x)
+            time += task_count if not maxHeap else n + 1
         
         return time
-            
 
 
+        # A, A, A, B, B, B
+        # A: 3
+        # B: 3
+        # [(-3, A), (-3, B)]
+        # A, B, idle, A, B, idle
+
+        
         

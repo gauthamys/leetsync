13f4289466352764695b301083446daca488cd64
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-03-06 23:06:48 -0600

    Time: 220 ms (29.30%) | Memory: 41.8 MB (43.22%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -1,11 +1,9 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        times = sorted(zip(position, speed), key=lambda x: -x[0])
-        stk = []
-        for p, s in times:
+        stk = [] # time reached
+        for p, s in sorted(zip(position, speed), key=lambda x: -x[0]):
             t = (target - p) / s
-            if stk and t <= stk[-1]:
+            if stk and stk[-1] >= t:
                 continue
-            else:
-                stk.append(t)
+            stk.append(t)
         return len(stk)

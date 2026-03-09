fabc90b2f8ea1e38f4589e063797f1a19c18f0f3
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-03-09 15:37:26 -0500

    Time: 11 ms (68.72%) | Memory: 19.7 MB (88.15%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,31 +1,31 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        res = []
         cols = set()
         diag = set()
         anti = set()
-        board = [['.'] * n for _ in range(n)]
+        res = []
+        cur = [['.'] * n for _ in range(n)]
 
         def backtrack(r):
             if r == n:
-                res.append([''.join(row) for row in board])
+                res.append([''.join(row) for row in cur])
                 return
             
             for c in range(n):
-                if c in cols or (r - c) in diag or (r + c) in anti:
+                if c in cols or (r + c) in anti or (r - c) in diag:
                     continue
-                
                 cols.add(c)
-                diag.add(r - c)
-                anti.add(r + c)
-                board[r][c] = 'Q'
+                diag.add((r - c))
+                anti.add((r + c))
 
+                cur[r][c] = 'Q'
                 backtrack(r + 1)
+                cur[r][c] = '.'
 
                 cols.remove(c)
-                diag.remove(r - c)
-                anti.remove(r + c)
-                board[r][c] = '.'
+                diag.remove((r - c))
+                anti.remove((r + c))
         
         backtrack(0)
         return res
+                

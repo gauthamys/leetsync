c2c5972c852e5734eee0a807ba249861b101de38
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-03-09 19:31:23 -0500

    Time: 0 ms (100.00%) | Memory: 20.5 MB (71.57%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -5,8 +5,8 @@ class Solution:
             mid = (l + r) // 2
             if nums[mid] == target:
                 return mid
-            elif nums[mid] > target:
-                r = mid - 1
-            else:
+            elif nums[mid] < target:
                 l = mid + 1
+            else:
+                r = mid - 1
         return -1

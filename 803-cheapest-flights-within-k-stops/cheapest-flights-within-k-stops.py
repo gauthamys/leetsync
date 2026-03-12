5dca35daf91bce243e07897aeccb2da784fa97b4
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-03-12 13:42:46 -0500

    Time: 27 ms (45.77%) | Memory: 20.1 MB (91.23%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -2,11 +2,13 @@ class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         prices = [float('inf')] * n
         prices[src] = 0
+
         for _ in range(k + 1):
-            tmpPrices = prices.copy()
-            for s, d, p in flights:
-                if prices[s] + p < tmpPrices[d]:
-                    tmpPrices[d] = prices[s] + p
-            prices = tmpPrices
+            tmp = prices.copy()
+            for u, v, w in flights:
+                if prices[u] + w < tmp[v]:
+                    tmp[v] = prices[u] + w
+            prices = tmp
         
         return prices[dst] if prices[dst] != float('inf') else -1
+            

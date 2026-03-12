28eaf8d413bf44c250f9685db7638d691298e5d9
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-03-12 14:36:54 -0500

    Time: 107 ms (33.97%) | Memory: 23.6 MB (40.71%) - LeetSync

---
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        dictionary = set(dictionary)
+        dp = {len(s): 0}
+
+        def dfs(i):
+            if i in dp:
+                return dp[i]
+            
+            res = 1 + dfs(i + 1) # skipping current char
+            for j in range(i, len(s)):
+                if s[i:j + 1] in dictionary:
+                    res = min(res, dfs(j + 1))
+            
+            dp[i] = res
+            return res
+        
+        return dfs(0)

4b29d581994b5ad572e1149e6b97f378826efc41
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-03-11 23:06:49 -0500

    Time: 176 ms (26.59%) | Memory: 74.1 MB (31.91%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -1,18 +1,14 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         dp = {}
-        def backtrack(start, curSum):
+        def dfs(start, curSum):
             if start == len(nums):
                 return 1 if curSum == target else 0
             
             if (start, curSum) in dp:
                 return dp[(start, curSum)]
-
-            dp[(start, curSum)] = backtrack(start + 1, curSum + nums[start]) + backtrack(start + 1, curSum - nums[start])
-
+            
+            dp[(start, curSum)] = dfs(start + 1, curSum + nums[start]) + dfs(start + 1, curSum - nums[start])
             return dp[(start, curSum)]
         
-        return backtrack(0, 0)
-
-            
-            
+        return dfs(0, 0)

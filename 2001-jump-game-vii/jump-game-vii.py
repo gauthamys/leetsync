9133f4cf642646309b1c0e4d5e461cd27b07eccd
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-03-11 19:03:26 -0500

    Time: 124 ms (91.73%) | Memory: 23.8 MB (52.73%) - LeetSync

---
+++ 2001-jump-game-vii/jump-game-vii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
+        if s[-1] == '1':
+            return False
+        
+        q, farthest = deque([0]), 0
+        while q:
+            i = q.popleft()
+            start = max(i + minJump, farthest + 1)
+            for j in range(start, min(len(s), i + maxJump + 1)):
+                if s[j] == '0':
+                    if j == len(s) - 1:
+                        return True
+                    q.append(j)
+            farthest = i + maxJump 
+                
+
+        return False

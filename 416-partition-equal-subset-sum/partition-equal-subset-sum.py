92939ae75ae894ec08bc25f4c1b7aabcef10524b
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-03-11 23:08:29 -0500

    Time: 217 ms (94.51%) | Memory: 21 MB (52.75%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -3,12 +3,10 @@ class Solution:
         total = sum(nums)
         if total % 2:
             return False
+            
         target = total // 2
         sums = set([0])
-        for i in range(len(nums) - 1, -1, -1):
-            if nums[i] == target:
-                return True
-
+        for i in range(len(nums)):
             for s in sums.copy():
                 if s + nums[i] == target:
                     return True

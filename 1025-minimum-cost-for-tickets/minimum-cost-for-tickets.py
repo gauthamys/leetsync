07ce1e57c620ed50863db0f851ae320604b72111
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-03-12 20:55:34 -0500

    Time: 0 ms (100.00%) | Memory: 19.1 MB (99.36%) - LeetSync

---
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        lastDay = days[-1]
+        dp = [0] * (lastDay + 1)
+        i = 0
+
+        for day in range(1, lastDay + 1):
+            if day < days[i]:
+                dp[day] = dp[day - 1]
+            else:
+                i += 1
+                dp[day] = min(dp[day - 1] + costs[0], dp[max(0, day - 7)] + costs[1], dp[max(0, day - 30)] + costs[2])
+        
+        return dp[lastDay]

69bca9e1e0caa6ab9dab884ac7ea121636b06b97
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-03-15 01:17:09 -0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (14.97%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,13 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n == 1:
-            return 1
-            
-        d = [0] * (n + 1)
-        d[1] = 1
-        d[2] = 2
-
-        for i in range(3, (n+1)):
-            d[i] = d[i - 1] + d[i - 2]
-        
-        return d[-1]
+        @lru_cache(None)
+        def helper(s):
+            if s == 1:
+                return 1
+            if s == 2:
+                return 2
+            return helper(s - 1) + helper(s - 2)
+        return helper(n)

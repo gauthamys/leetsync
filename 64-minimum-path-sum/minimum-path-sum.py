85ba481eab43c40546f4863445ac1f4ae7ed545d
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-03-15 01:40:42 -0500

    Time: 32 ms (16.60%) | Memory: 32.6 MB (5.32%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,16 +1,15 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        memo = {(0, 0): grid[0][0]}
+        @lru_cache(None)
         def dfs(i, j):
-            if (i, j) in memo:
-                return memo[(i, j)]
+            if i == 0 and j == 0:
+                return grid[0][0]
             if i == 0:
                 return grid[i][j]  + dfs(i, j - 1)
             if j == 0:
                 return grid[i][j] + dfs(i - 1, j)
-            memo[(i, j)] = grid[i][j] + min(dfs(i - 1, j), dfs(i, j - 1))
-            return memo[(i, j)]
-        
+            
+            return grid[i][j] + min(dfs(i - 1, j), dfs(i, j - 1))
         rows, cols = len(grid), len(grid[0])
         return dfs(rows - 1, cols - 1)
             

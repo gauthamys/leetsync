86c96d6126393b6b71648aded755bfaa3160ac57
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-03-15 01:32:20 -0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (69.40%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,9 +1,11 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        @lru_cache(None)
-        def dfs(i, j):
-            if i == 0 or j == 0:
-                return 1
-            return dfs(i - 1, j) + dfs(i, j - 1)
-        
-        return dfs(n - 1, m - 1)
+        dp = [[0] * m for _ in range(n)]
+        for i in range(n):
+            dp[i][0] = 1
+        for j in range(m):
+            dp[0][j] = 1
+        for i in range(1, n):
+            for j in range(1, m):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        return dp[n - 1][m - 1]

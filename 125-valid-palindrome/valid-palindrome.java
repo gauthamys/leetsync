13486c33f08fa23d238aeae617ea180314dd198a
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2026-07-21 01:05:32 -0500

    Time: 2 ms (99.29%) | Memory: 44.3 MB (65.49%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,25 +1,25 @@
-public class Solution {
+class Solution {
     public boolean isPalindrome(String s) {
-        int l = 0, r = s.length() - 1;
-
+        char[] charArray = s.toCharArray();
+        int l = 0;
+        int r = s.length() - 1;
         while (l < r) {
-            while (l < r && !alphaNum(s.charAt(l))) {
+            char leftChar = s.charAt(l);
+            char rightChar = s.charAt(r);
+            if (l < r && !Character.isLetterOrDigit(leftChar)) {
                 l++;
+                continue;
             }
-            while (r > l && !alphaNum(s.charAt(r))) {
+            if(l < r && !Character.isLetterOrDigit(rightChar)) {
                 r--;
+                continue;
             }
-            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
+            if(Character.toLowerCase(leftChar) != Character.toLowerCase(rightChar)) {
                 return false;
             }
-            l++; r--;
+            l++;
+            r--;
         }
         return true;
-    }
-
-    public boolean alphaNum(char c) {
-        return (c >= 'A' && c <= 'Z' ||
-                c >= 'a' && c <= 'z' ||
-                c >= '0' && c <= '9');
     }
 }
